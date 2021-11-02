49dce2b44db46069be51eea8816ef2119da060df
Scott Zhao <zhaomh1998@outlook.com>
2021-11-02 10:09:56 -0400

    basic things working

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,58 @@
+class ListingsController < ApplicationController
+
+  def show
+    id = params[:id] # retrieve listing ID from URI route
+    @listing = Listing.find(id) # look up listing by unique ID
+    # will render app/views/Listings/show.<extension> by default
+  end
+
+  def index
+    sort = params[:sort] || session[:sort]
+    case sort
+    when 'name'
+      ordering, @name_header_css = { :name => :asc }, 'bg-warning hilite'
+    when 'purchase_date'
+      ordering, @date_header_css = { :purchase_date => :asc }, 'bg-warning hilite'
+    end
+
+    if params[:sort] != session[:sort]
+      session[:sort] = sort
+      redirect_to :sort => sort and return
+    end
+
+    if sort
+      @listings = Listing.all.order(ordering)
+    else
+      @listings = Listing.all
+    end
+  end
+
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+    @listing = Listing.create!(params[:listing])
+    flash[:notice] = "#{@listing.name} was successfully created."
+    redirect_to listings_path
+  end
+
+  def edit
+    @listing = Listing.find params[:id]
+  end
+
+  def update
+    @listing = Listing.find params[:id]
+    @listing.update_attributes!(params[:listing])
+    flash[:notice] = "#{@listing.name} was successfully updated."
+    redirect_to listing_path(@listing)
+  end
+
+  def destroy
+    @listing = Listing.find(params[:id])
+    @listing.destroy
+    flash[:notice] = "Listing '#{@listing.name}' deleted."
+    redirect_to listings_path
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rottenpotatoes::Application.routes.draw do
-  resources :movies
-  # map '/' to be a redirect to '/movies'
-  root :to => redirect('/movies')
+  resources :listings
+  # map '/' to be a redirect to '/listings'
+  root :to => redirect('/listings')
 end
