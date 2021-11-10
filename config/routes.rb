2c1d4af05d09e4477232a5b1f810ec42e9b39044
Scott Zhao <zhaomh1998@outlook.com>
2021-11-10 12:11:20 -0500

    update application name

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rottenpotatoes::Application.routes.draw do
+InstaFurniture::Application.routes.draw do
   resources :listings
   # map '/' to be a redirect to '/listings'
   root :to => redirect('/listings')
