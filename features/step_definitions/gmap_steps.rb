6f0aab768e15e3501e39b901c54c40438c400868
Yizhuo Wu <yizhuw1@uci.edu>
2021-11-17 22:21:42 -0500

    update gmap features

--- features/step_definitions/gmap_steps.rb
+++ features/step_definitions/gmap_steps.rb
@@ -1,10 +1,9 @@
-Given(/^I am on the homepage$/) do
-  visit root_path
-end
-
 Then(/^I should see a map on the webpage/) do
-  #expect(page).to find('#map', wait:100)
-  page.has_css?('#map', wait:100)
+  parent = page.find('div#main')
+  expect(parent). to have_css("#map", visible:false)
+  expect(page.has_css?('#map', visible: false)).to eq true
 end
 
-
+Then(/^I should see different markers on the webpage/) do
+    expect(JSON.load(page.body.match(/<script>\s+var items = (\[.*\])/).captures[0])) != nil
+end
