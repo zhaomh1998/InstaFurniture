c3d45a2f7b157075ae1874d6c8270e9a9892527f
Scott Zhao <zhaomh1998@outlook.com>
2021-11-17 21:24:10 -0500

    set different callback url for prod / dev

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,12 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-	OmniAuth.config.allowed_request_methods = [:post, :get]
+  OmniAuth.config.allowed_request_methods = [:post, :get]
+  if ENV.has_key?("DEVELOPMENT")
     provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
-        redirect_uri:"http://localhost:3000/auth/google_oauth2/callback"
+      redirect_uri: "http://localhost:3000/auth/google_oauth2/callback"
     }
+  else
+    provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
+      redirect_uri: "http://evening-atoll-94372.herokuapp.com:3000/auth/google_oauth2/callback"
+    }
+  end
 end
