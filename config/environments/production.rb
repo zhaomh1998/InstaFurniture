3e77b1f06d25c754c50a2284920d6062991b46b5
Scott Zhao <zhaomh1998@outlook.com>
2021-12-03 18:13:55 -0500

    add ActiveStorage configuration to production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,4 +60,7 @@ InstaFurniture::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  # ActiveStorage
+  config.active_storage.service = :local
 end
