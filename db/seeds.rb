697c7f6b1f8b25b0134059fee3dc3ac5cca2dbdd
Scott Zhao <zhaomh1998@outlook.com>
2021-12-05 00:56:33 -0500

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,54 +7,140 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 listings = [
-  { :name => 'Mission Oak II Queen Bookcase Storage Bed',
-    :description => 'In decent condition, one slat broken. Very heavy and sturdy bed with storage. Must come pick up!
-Will need to partially disassemble to remove from room. Just way too big to fit in my new room. Originally $800.',
+  { :name => 'IKEA Side Table',
+    :description => 'Free. Must pick up from UWS. Originally purchased from IKEA. https://www.ikea.com/us/en/p/lunnarp-side-table-brown-60399030/',
     :location => '2549 Broadway, New York, NY 10025',
     :purchase_date => '10-Oct-2019',
     :elevator_building => 1,
     :pickup_only => 1,
-    :price => 300
+    :price => 0,
+    :image => 'seed-1.jpg'
   },
   {
-    :name => 'Electric Kettle, Brentwood',
-    :description => 'Brentwood electric kettle, good condition. Speed boiling, auto shut-off, boil-protection. Original
-price $37.95. Used two years. Now sell for $10. PICK UP ONLY around 114th st & Broadway.',
+    :name => 'Ikea Desk',
+    :description =>
+      "Ikea table, lightly used for only 6 months
+43.25” W x 26.5” D x 29” H
+Pick up in elevator building
+
+Closest item on Ikea's website is:
+https://www.ikea.com/us/en/p/lagkapten-adils-desk-black-brown-black-s49417021/#content",
     :location => '500 West 120th Street, New York, NY 10027',
     :purchase_date => '5-Dec-2020',
-    :elevator_building => 0,
-    :pickup_only => 0,
-    :price => 10
-  },
-  {
-    :name => 'Sofa bed with a pull out mattress',
-    :description => 'Sofa bed with a pull out mattress between full and queen size, very comfortable and with 3 pillows  - 250$ ( originally 650$)',
-    :location => '400 W 119th St, New York, NY 10027-7109',
-    :purchase_date => '15-Jun-2018',
     :elevator_building => 1,
     :pickup_only => 1,
-    :price => 250
+    :price => 30,
+    :image => 'seed-2.jpg'
   },
   {
-    :name => 'Wayfair Bench with storage',
-    :description => 'very good condition, one year old barely used 60$ (originally 180$)',
+    :name => 'Aeron Chair Herman Miller Size B',
+    :description => 'The seat is broken and torn and needs to be replaced. Other than that everything is in a good condition. If you are lucky you can buy a seat frame and mesh on ebay for up to $150 and easily install it yourself. There are also spare parts on marketplace for a good price.',
+    :location => '400 W 119th St, New York, NY 10027-7109',
+    :purchase_date => '15-Jun-2017',
+    :elevator_building => 1,
+    :pickup_only => 1,
+    :price => 140,
+    :image => 'seed-3.jpg'
+  },
+  {
+    :name => 'Used Office Chair',
+    :description => 'Used home office chair. All functionality still works, height adjustment and lean adjustment.',
     :location => '103 West 107th Street, New York',
-    :purchase_date => '20-Nov-2020',
+    :purchase_date => '20-Nov-2018',
     :elevator_building => 1,
     :pickup_only => 0,
-    :price => 60
+    :price => 25,
+    :image => 'seed-4.jpg'
   },
   {
-    :name => 'Desk and desk chair',
-    :description => 'Black Ikea Desk - $35 Way fair desk chair: $40 Prices negotiable! Pick up only!',
+    :name => 'Small Desk for Home Office',
+    :description => "Light wood desk with white rims.
+Sufficient space for laptop and notepad.
+Lightweight so it's convenient and easy to carry and transport.
+Clean and very good condition.
+32\" width x 32.5\" heigh x 20\" depth.
+Pick up only",
     :location => '302 West 102nd Street, New York',
     :purchase_date => '19-Mar-2018',
-    :elevator_building => 0,
+    :elevator_building => 1,
     :pickup_only => 1,
-    :price => 35
+    :price => 35,
+    :image => 'seed-5.jpg'
+  },
+  {
+    :name => 'Target office chair',
+    :description => "In good condition. Pickup in UWS",
+    :location => '300 West 96th Street, New York',
+    :purchase_date => '19-Mar-2017',
+    :elevator_building => 1,
+    :pickup_only => 1,
+    :price => 40,
+    :image => 'seed-6.jpg'
+  },
+  {
+    :name => 'MUJI Wooden Full Size Bed Frame',
+    :description => "Fits a full size mattress. Nice, sturdy wooden frame first purchased from MUJI in 2015 for $600. See pictured for cosmetic damage from apartment moves. Additional cosmetic damage on the headboard from a puppy on a chewing binge. May come with some dog fur hiding in some spots from aforementioned puppy. Bed is currently disassembled and could likely fit in some cars for pick up. Pick up only.",
+    :location => '300 West 125th Street, New York',
+    :purchase_date => '19-Mar-2015',
+    :elevator_building => 1,
+    :pickup_only => 1,
+    :price => 100,
+    :image => 'seed-7.jpg'
+  },
+  {
+    :name => 'Ikea dresser 4 drawers 40-31-19',
+    :description => "Yeah you’re looking at Ikea mom dresser with four drawers good condition all drawers open and close properly.
+Size of this dresser is 41 inch height 31 inch wide and an 19 inch depth.
+Available for pick up or I can have delivered locally for a small fee",
+    :location => '200 West 120th Street, New York',
+    :purchase_date => '19-Mar-2019',
+    :elevator_building => 1,
+    :pickup_only => 0,
+    :price => 100,
+    :image => 'seed-8.jpg'
+  },
+  {
+    :name => 'Haugsvär Ikea Mattress.',
+    :description => "Haugsvär mattress from Ikea. In good condition. Definitely on the soft/comfy side. Let me know if you're interested!",
+    :location => '610 West 111th Street, New York',
+    :purchase_date => '19-Mar-2018',
+    :elevator_building => 1,
+    :pickup_only => 1,
+    :price => 200,
+    :image => 'seed-9.jpg'
+  },
+  {
+    :name => 'IKEA Sofa',
+    :description => "Small white sofa with 2 different covers included.
+IKEA Klippan Faux Leather Loveseat Sofa
+Length: 69”
+Width: 35”
+Height: 26”",
+    :location => '400 West 119th Street, New York',
+    :purchase_date => '19-Mar-2018',
+    :elevator_building => 1,
+    :pickup_only => 1,
+    :price => 50,
+    :image => 'seed-10.jpg'
+  },
+  {
+    :name => 'Sofa',
+    :description => "",
+    :location => '615 West 113th Street, New York',
+    :purchase_date => '19-Mar-2016',
+    :elevator_building => 1,
+    :pickup_only => 0,
+    :price => 150,
+    :image => 'seed-11.jpg'
   },
 ]
 
 listings.each do |listing|
-  Listing.create!(listing)
+  entry = Listing.create!(listing.except(:image))
+  if listing.has_key?(:image)
+    entry.photo.attach(
+      io: File.open("db/seed-resources/#{listing[:image]}"),
+      filename: listing[:image]
+    )
+  end
 end
