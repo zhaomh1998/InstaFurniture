39017e7e99768cc9a6d1f2fd2415e459bd2ce3d6
Scott Zhao <zhaomh1998@outlook.com>
2021-12-04 19:52:31 -0500

    update login failure handle

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,12 +3,14 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   if ENV.has_key?("DEVELOPMENT")
     puts "OAuth set up for development"
     provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
-      redirect_uri: "http://localhost:3000/auth/google_oauth2/callback"
+      redirect_uri: "http://localhost:3000/auth/google_oauth2/callback",
+      provider_ignores_state: true
     }
   else
     puts "OAuth set up for production"
     provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
-      redirect_uri: "https://evening-atoll-94372.herokuapp.com/auth/google_oauth2/callback"
+      redirect_uri: "https://insta-furniture.herokuapp.com/auth/google_oauth2/callback",
+      provider_ignores_state: true
     }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,8 @@ InstaFurniture::Application.routes.draw do
   # user login
   resources :users
 
-  # 'auth/:provider/callback' :to => 'sessions#create'
-  # get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/:provider/callback' => 'sessions#create', :as => 'login'
   get '/mock_login' => 'sessions#create_mock', :as => 'mock_login'
   get '/logout' => 'sessions#logout', :as => 'logout'
+  get 'auth/failure' => 'sessions#error', :as => 'login_fail'
 end
