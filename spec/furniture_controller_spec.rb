46f4fbdeb9addeb7eff79ef18a29bfcd5ebd66cd
Scott Zhao <zhaomh1998@outlook.com>
2021-12-11 00:42:49 -0500

    update old listings rspec to pass

--- spec/furniture_controller_spec.rb
+++ spec/furniture_controller_spec.rb
@@ -1,36 +1,13 @@
 require 'rails_helper'
 
-if RUBY_VERSION >= '2.6.0'
-  if Rails.version < '5'
-    class ActionController::TestResponse < ActionDispatch::TestResponse
-      def recycle!
-        # hack to avoid MonitorMixin double-initialize error:
-        @mon_mutex_owner_object_id = nil
-        @mon_mutex = nil
-        initialize
-      end
-    end
-  else
-    puts "Monkeypatch for ActionController::TestResponse no longer needed"
-  end
-end
-
 RSpec.describe ListingsController, :type => :controller do
   describe "listings" do
     it "shows listings on index page" do
-      Listing.delete_all
-      listing1 = { :name => "listing1", :description => "lorem", :elevator_building => "true", :pickup_only => "true", :purchase_date => "2017-07-20" }
-      listing2 = { :name => "listing2", :description => "ipsum", :elevator_building => "true", :pickup_only => "true", :purchase_date => "2017-07-20" }
-      Listing.create(listing1)
-      Listing.create(listing2)
       get :index
       expect(response).to render_template("listings/index")
     end
 
     it "renders listing detail page" do
-      Listing.delete_all
-      listing1 = { :name => "listing1", :description => "lorem", :elevator_building => "true", :pickup_only => "true", :purchase_date => "2017-07-20" }
-      Listing.create(listing1)
       get :show, params: { id: 1 }
       expect(response).to render_template("listings/show")
     end
@@ -43,42 +20,52 @@ RSpec.describe ListingsController, :type => :controller do
     end
 
     it "submit listing" do
-      Listing.delete_all
       post :create, params: { listing: { :name => "listing1", :description => "lorem", :elevator_building => "true", :pickup_only => "true", :purchase_date => "2017-07-20" } }
       expect(response).to redirect_to listings_path
-      expect(flash[:notice]).to match(/listing1 was successfully created./)
+      expect(flash[:success]).to match(/listing1 was successfully created./)
     end
   end
 
   describe "edit existing listings" do
     it "renders the edit page" do
-      Listing.delete_all
-      listing1 = { :name => "listing1", :description => "ipsum", :elevator_building => "true", :pickup_only => "true", :purchase_date => "2017-07-20" }
-      Listing.create(listing1)
+      # Editing my own listing should succeed
       get :edit, params: { id: 1 }
       expect(response).to render_template("listings/edit")
+
+      # Editing someone else's listing should fail and redirect to homepage
+      get :edit, params: {id: 2}
+      expect(response).to redirect_to listings_path
+      expect(flash[:notice]).to match(/Error: You are not allowed to edit someone else's listing!/)
     end
 
     it "submit edits" do
-      Listing.delete_all
-      listing1 = { :name => "listing1", :description => "lorem", :elevator_building => "true", :pickup_only => "true", :purchase_date => "2017-07-20" }
-      Listing.create(listing1)
+      # Editing my own listing should succeed
       get :update, params: { id: 1,
                              listing: { :name => "listing1_updated", :description => "ipsum", :elevator_building => "true", :pickup_only => "true", :purchase_date => "2017-07-20" }
       }
       expect(response).to redirect_to listing_path(1)
-      expect(flash[:notice]).to match(/listing1_updated was successfully updated./)
+      expect(flash[:success]).to match(/listing1_updated was successfully updated./)
+
+      # Editing someone else's listing should fail and redirect to homepage
+      get :update, params: { id: 2,
+                             listing: { :name => "listing2_updated", :description => "ipsum", :elevator_building => "true", :pickup_only => "true", :purchase_date => "2017-07-20" }
+      }
+      expect(response).to redirect_to listings_path
+      expect(flash[:notice]).to match(/Error: You are not allowed to edit someone else's listing!/)
     end
   end
 
   describe "delete existing listings" do
     it "redirects to the index page" do
-      Listing.delete_all
-      listing1 = { :name => "listing1", :description => "lorem", :elevator_building => "true", :pickup_only => "true", :purchase_date => "2017-07-20" }
-      Listing.create(listing1)
+      # Deleting my own listing should succeed
       delete :destroy, params: { id: 1 }
       expect(response).to redirect_to listings_path
-      expect(flash[:notice]).to match(/Listing 'listing1' deleted./)
+      expect(flash[:success]).to match(/Listing 'IKEA Side Table' deleted./)
+
+      # Deleting someone else's listing should fail
+      delete :destroy, params: { id: 2 }
+      expect(response).to redirect_to listings_path
+      expect(flash[:notice]).to match(/Error: You are not allowed to delete someone else's listing!/)
     end
   end
 
